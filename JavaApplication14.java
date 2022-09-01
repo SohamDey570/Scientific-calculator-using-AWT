51f58699e5c4e8f3482622034957376278bbeee0
Your Name <you@example.com>
2022-09-01 23:20:34 +0530

    Added my project

---
+++ JavaApplication14.java
@@ -0,0 +1,13 @@
+package javaapplication14;
+public class JavaApplication14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            
+Calculator sc = new Calculator();
+
+    }
+    
+}
