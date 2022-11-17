e1483347482ae989dccfea51f8ed93c9a5ac3d30
SohamDey570 <104816597+SohamDey570@users.noreply.github.com>
2022-11-17 20:32:12 +0530

    Update JavaApplication14.java

--- JavaApplication14.java
+++ JavaApplication14.java
@@ -1,9 +1,6 @@
 package javaapplication14;
 public class JavaApplication14 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
             
 Calculator sc = new Calculator();
